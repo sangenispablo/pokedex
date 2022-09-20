4185714f85549c2a4acc3e71d902ab0c8f7a8bd4
Pablo Sangenis <pasangenis@teco.com.ar>
2022-09-20 15:48:24 -0300

    Insertando de modo tradicional en executeSeed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
 
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
+      await this.pokemonModel.create({ name, no });
       console.log({ name, no });
     });
-    return data.results;
+    return 'Seed ejecutado';
   }
 }
